12983d8885f07a480268655f2f2f43a4d13b4e06
md uddin <mduddin1998@yahoo.com>
2021-01-06 06:42:20 -0500

    added wiki search widget

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,60 @@
 import React, { useEffect, useState } from "react";
+import DOMPurify from "dompurify";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
   const [searchResponse, setSearchResponse] = useState([]);
 
   useEffect(() => {
-    console.log("I only run once");
-  }, []);
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setSearchResponse(data.query.search);
+    };
+
+    if (term && !searchResponse.length) search();
+
+    const toId = setTimeout(() => {
+      if (term) search();
+    }, 500);
+
+    return () => {
+      clearTimeout(toId);
+    };
+  }, [term, searchResponse.length]);
+
+  const renderedResults = searchResponse.map((search) => {
+    return (
+      <div className="item" key={search.title}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${search.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{search.title}</div>
+          <span
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(search.snippet),
+            }}
+          ></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -20,6 +68,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
